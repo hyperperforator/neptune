c010d3375cf08008617b8e11684abe02d228533f
Owen Williams <hi@owened.co.nz>
2014-08-27 12:27:38 +1200

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'owenwillia.ms'
+set :repo_url, 'git@github.com:hyperperforator/neptune.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+ 
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      node_stop = "cd #{current_path} && forever stop 0;true;"
+      node_start = "cd #{current_path} && forever app.js;true"
+      execute node_start
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
